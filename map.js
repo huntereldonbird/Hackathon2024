79d799e1261dbbce4b0a5e131fe26ccb6b3b0804
HyrumHendrickson <hendrickson.hyrum@gmail.com>
2024-10-19 14:12:48 -0600

    fixed colors

--- map.js
+++ map.js
@@ -14,13 +14,73 @@ map.getPane('fixedLabels').style.pointerEvents = 'none';
 // Store all label markers
 var labelMarkers = [];
 
+
+/*
+function createAndUpdateCSS(color, id) {
+    // Create a new <style> element
+    var styleElement = document.createElement('style');
+    console.log(document.getElementsByClassName("building-label"));
+    // Add the CSS rules to the <style> element
+    console.log("["+id +"]");
+    console.log(color);
+    console.log("");
+    styleElement.innerHTML = `
+        #${id} {
+            background-color: ${color};
+            
+        }
+    `;
+
+    // Append the <style> element to the <head> element
+    document.head.appendChild(styleElement);
+}
+
+
+*/
+
+
+
+
+    
+
+
+
+
+
+
+
+// Call the createAndUpdateCSS() function to create and update the CSS file
+
+
+
+
 // Function to add fixed building labels
-function addFixedBuildingLabel(lat, lng, text, insideText) {
+function addFixedBuildingLabel(lat, lng, text, insideText, color) {
+
+     // Get the existing .building-label element
+     //let buildingLabel = document.querySelector('.building-label');
+
+     // Modify the existing styles directly
+     //console.log(buildingLabel.color)
+
+
+
     var labelIcon = L.divIcon({
         className: 'building-label',
-        html: text,
-        iconSize: [55, 35],
-        iconAnchor: [50, 10]
+        html:  `
+        <div style="
+            width: 100%; 
+            height: 100%; 
+            border-radius: 5px; 
+            text-align: center;
+            background-color: ${color};
+            ">
+            ${text} ppl 
+        </div>
+    `,
+        iconSize: [70, 45],
+        iconAnchor: [50, 10],
+        
     });
 
     var marker = L.marker([lat, lng], {
@@ -57,8 +117,10 @@ function updateLabelsFromFile(filePath) {
             const lines = data.split('\n');
             lines.forEach(line => {
                 const [name, lat, lng, inside] = line.split(',');
-                if (name && lat && lng) {
-                    addFixedBuildingLabel(parseFloat(lat), parseFloat(lng), name.replace("|",""), inside.replace("|","")).addTo(map);
+                if (name && lat && lng) {  
+                    var color = "green";
+                    if(name.replace("|","").split(" ")[1] > 7) {color = "red";}
+                    addFixedBuildingLabel(parseFloat(lat), parseFloat(lng), name.replace("|",""), inside.replace("|",""), color).addTo(map);
                 }
             });
         })
