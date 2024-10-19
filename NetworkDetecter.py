0ea272c1a012a12d92eb09a3f4d6251c1f0b5776
EldonBird <Birdheldon@gmail.com>
2024-10-19 11:41:54 -0600

    Fixed the netwrok stuff, may need more changes to updater

--- NetworkDetecter.py
+++ NetworkDetecter.py
@@ -1,6 +1,7 @@
 import time, subprocess;
 
 
+# scans individual network
 def scan_network():
     result = subprocess.run(["sudo", "arp-scan", "192.168.0.1/24"], capture_output=True, text=True)
     devices = []
@@ -14,6 +15,8 @@ def scan_network():
 
 
 
+# Get devices from spesific AP 
+# Net is the 192.168.0.1/24 so if you want to use this function it is the same 
 def Get_Devices_From_AP(net):
     devices = []
     result = subprocess.run(["sudo", "arp-scan", net], capture_output=True, text=True)
@@ -28,7 +31,11 @@ def Get_Devices_From_AP(net):
     return devices
 
 
-def get_number(net):
+def get_count(net):
     net = Get_Devices_From_AP(net)
     count = len(net) - 2
     return count
+
+
+
+
