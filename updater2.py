2f373386cb0f84e99ad2cad767a464e301d45e91
HyrumHendrickson <hendrickson.hyrum@gmail.com>
2024-10-19 15:10:18 -0600

    fixed updated2

--- updater2.py
+++ updater2.py
@@ -1,6 +1,13 @@
 import time
 import NetworkDetecter
 import sys
+import ac_test
+
+ocupancy = 5 #get_people_data()
+surface_area = 3000 # in sq ft
+room_internal_temp = 68 #in F
+room_external_temp = 68 #in f
+desired_temp = 68 #in f
 
 # Function to increment the contents in parentheses
 def increment_contents():
@@ -10,6 +17,7 @@ def increment_contents():
     for i, line in enumerate(lines):
         # Split the line into label and coordinates
         label, latitude, longitude, inside = line.strip().split(',')
+        hvac_difference = ac_test.cacl_HVAC_maintain(ocupancy, surface_area, room_external_temp,desired_temp)
 
         # Increment the contents in parentheses
         count = NetworkDetecter.get_count(arg)
@@ -20,15 +28,12 @@ def increment_contents():
         new_label = label.split('|')[0] + '|' + str(count)
 
         # Write the updated label and coordinates back to the file
-        lines[i] = new_label + ',' + latitude + ',' + longitude + "," + inside + '\n'
+        lines[i] = new_label + ',' + latitude + ',' + longitude + "," + inside.split("|")[0].split("=")[0] + "=" + str(hvac_difference) + '\n'
 
     with open('labels.txt', 'w') as file:
         file.writelines(lines)
 
-# Run the increment function every 2 seconds
-while True:
-    increment_contents()
-    time.sleep(2)
+
 
 
 
@@ -36,3 +41,9 @@ while True:
 if __name__ == "__main__":
     arg = sys.argv[1]
     print(arg)
+
+
+# Run the increment function every 2 seconds
+while True:
+    increment_contents()
+    time.sleep(2)
